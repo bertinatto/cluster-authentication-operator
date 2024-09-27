fd9f79b012aacc090ed521c3db4f6aa47a009a03
Fabio Bertinatto <fbertina@redhat.com>
2024-09-27 15:58:31 -0300

    add helper to support SSA migration

--- pkg/controllers/common/conditions.go
+++ pkg/controllers/common/conditions.go
@@ -8,6 +8,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 
 	operatorv1 "github.com/openshift/api/operator/v1"
+	applyoperatorv1 "github.com/openshift/client-go/operator/applyconfigurations/operator/v1"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
 )
 
@@ -103,4 +104,31 @@ func UpdateControllerConditions(ctx context.Context, operatorClient v1helpers.Op
 	}
 
 	return nil
+}
+
+func ApplyControllerConditions(ctx context.Context, operatorClient v1helpers.OperatorClient, fieldManager string, allConditionNames sets.String, updatedConditions []operatorv1.OperatorCondition) error {
+	status := applyoperatorv1.OperatorStatus()
+
+	for _, conditionType := range allConditionNames.List() {
+		// clean up existing updatedConditions
+		newCondition := operatorv1.OperatorCondition{
+			Type:   conditionType,
+			Status: operatorv1.ConditionFalse,
+		}
+		if strings.HasSuffix(conditionType, "Available") {
+			newCondition.Status = operatorv1.ConditionTrue
+		}
+
+		if condition := v1helpers.FindOperatorCondition(updatedConditions, conditionType); condition != nil {
+			newCondition = *condition
+		}
+
+		status = status.WithConditions(applyoperatorv1.OperatorCondition().
+			WithType(newCondition.Type).
+			WithStatus(newCondition.Status).
+			WithReason(newCondition.Reason).
+			WithMessage(newCondition.Message))
+	}
+
+	return operatorClient.ApplyOperatorStatus(ctx, fieldManager, status)
 }
